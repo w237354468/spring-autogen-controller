6fe25de65444673ea982452641ff8d0a86e320b9
w237354468 <w237354468@hotmail.com>
2024-06-25 10:34:45 +0800

    [edit] move main application's fold

--- lcdp-framework-entrance/src/main/java/org/lcdpframework/LcdpApplication.java
+++ lcdp-framework-entrance/src/main/java/org/lcdpframework/LcdpApplication.java
@@ -1,4 +1,4 @@
-package org.lcdpframework.app;
+package org.lcdpframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
