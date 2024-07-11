ce4ba949281f6412a7825619ed606d7ee47f8617
w237354468 <w237354468@hotmail.com>
2024-07-11 09:17:54 +0800

    [add] event util

--- lcdp-framework-service/src/main/java/org/lcdpframework/server/util/LcdpEventUtil.java
+++ lcdp-framework-service/src/main/java/org/lcdpframework/server/util/LcdpEventUtil.java
@@ -1,24 +1,34 @@
 package org.lcdpframework.server.util;
 
 import org.lcdpframework.server.event.LcdpApplicationEvent;
-import org.lcdpframework.server.log.LcdpLog;
+import org.lcdpframework.server.log.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
-import static org.lcdpframework.server.log.LcdpLog.LOGGER_TYPE.SYSTEM;
+import static org.lcdpframework.server.log.Log.LOGGER_TYPE.SYSTEM;
 
+@Component
 public class LcdpEventUtil implements ApplicationContextAware {
 
-    private static ApplicationContext applicationContext;
+    private static final Logger logger = LoggerFactory.getLogger(LcdpEventUtil.class);
+
+    public static ApplicationContext applicationContext;
 
     public static void publishEvent(LcdpApplicationEvent event) {
-        LcdpLog.printInfo(SYSTEM, "pbulish event with", event.getClass().getSimpleName());
+        if (applicationContext == null) {
+            logger.error("application context hasn't initialized yet");
+            return;
+        }
+        Log.info(SYSTEM, "publish event with {}", event.getClass().getSimpleName());
         applicationContext.publishEvent(event);
     }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
+        LcdpEventUtil.applicationContext = applicationContext;
     }
 }
